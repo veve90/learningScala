2a169fad9de61c0e14745cf1c8655c17d5da8aa2
Alina GHERMAN <veve90@users.noreply.github.com>
2016-06-26 23:30:08 +0200

    done

--- week1/recfun/src/main/scala/recfun/Main.scala
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,63 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int  Int = {
+		if (c == 0 || c == r) 1
+		else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  } 
+  
+   
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+	  def balance(chars: List[Char]): Boolean = {
+		def f(chars: List[Char], numOpens: Int): Boolean = {
+		  if (chars.isEmpty) {
+			numOpens == 0
+		  } else {
+			val h = chars.head
+			val n =
+			  if (h == '(') numOpens + 1
+			  else if (h == ')') numOpens - 1
+			  else numOpens
+			if (n >= 0) f(chars.tail, n)
+			else false
+		  }
+		}
+
+		f(chars, 0)
+	  }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+	    def f(maxCoin: List[(Int, Int)], count: Int): Int = {
+		  if (maxCoin.isEmpty) {
+			count
+		  } else {
+			val b = ListBuffer[(Int, Int)]()
+			var newCount = count
+			for ((lastMaxCoin, total) <- maxCoin) {
+			  if (total < money) {
+				for (c <- coins) {
+				  if (c >= lastMaxCoin) {
+					val e = (c, total + c)
+					b += e
+				  }
+				}
+			  } else if (total == money) {
+				newCount += 1
+			  }
+			}
+
+			f(b.toList, newCount)
+		  }
+		}
+
+		val b = coins.map { c => (c, c) }
+		f(b, 0)
+	  }	
+	}
   }
